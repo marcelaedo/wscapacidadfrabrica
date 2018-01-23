40f5a0b4c3c712262a4d23b04c20b6af07e6c07f
marcelaedo <m.aedo04@ufromail.cl>
2018-01-23 14:25:27 -0300

    respaldo

--- ObtenerCapacidadFabrica/src/main/java/cl/akzio/auth/central/obtenercapacidadfabrica/provider/clienteEasyRedmine.java
+++ ObtenerCapacidadFabrica/src/main/java/cl/akzio/auth/central/obtenercapacidadfabrica/provider/clienteEasyRedmine.java
@@ -9,8 +9,13 @@ import javax.ws.rs.core.MediaType;
 
 public class clienteEasyRedmine {
 
-	Client client = ClientBuilder.newClient();
-	Response response = client.target("http://www.easyredmine.com/time_entries.xml?project_id=project_id&issue_id=issue_id&period_type=period_type&period=period&from=from&to=to&user_id=user_id&offset=offset&limit=limit&page=page")
-	  .request(MediaType.TEXT_PLAIN_TYPE)
-	  .get();
+
+Client client = ClientBuilder.newClient();
+Response response = client.target("https://private-anon-bbafd78696-easyredmine.apiary-proxy.com/time_entries.xml?project_id=project_id&issue_id=issue_id&period_type=period_type&period=period&from=from&to=to&user_id=user_id&offset=offset&limit=limit&page=page")
+  .request(MediaType.TEXT_PLAIN_TYPE)
+  .get();
+
+
+	
+	
 }
